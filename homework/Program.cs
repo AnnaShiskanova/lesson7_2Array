1fe076e05f3ab0bdf41b785cae203012cb119f40
Anna <shiskanovaag@mail.ru>
2023-03-04 13:07:59 +1000

    add task50

--- homework/Program.cs
+++ homework/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.Clear();
 Console.WriteLine("1 - Task 47");
-// Console.WriteLine("2 - Task 48");
-// Console.WriteLine("3 - Task 49");
-// Console.WriteLine("4 - Task 51");
-// Console.WriteLine("5 - Task 50");
+Console.WriteLine("2 - Task 50");
+// // Console.WriteLine("3 - Task 49");
+// // Console.WriteLine("4 - Task 51");
+// // Console.WriteLine("5 - Task 50");
 
 int NumberOfTask = Prompt("Введите номер задачи");
 switch(NumberOfTask)
@@ -13,30 +13,30 @@ switch(NumberOfTask)
     Task47();
     break;
 
-    // case 2:
-    // Console.Clear();
-    // Task48();
-    // break;
+    case 2:
+    Console.Clear();
+    Task50();
+    break;
 
-    // case 3:
-    // Console.Clear();
-    // Task49();
-    // break;
+//     // case 3:
+//     // Console.Clear();
+//     // Task49();
+//     // break;
 
-    // case 4:
-    // Console.Clear();
-    // Task51();
-    // break;
+//     // case 4:
+//     // Console.Clear();
+//     // Task51();
+//     // break;
 
-    // case 5:
-    // Console.Clear();
-    // Task50();
-    // break;
+//     // case 5:
+//     // Console.Clear();
+//     // Task50();
+//     // break;
 
         
-    // default:
-    // Console.WriteLine("Номер задачи введен неверно");
-    // break;
+//     // default:
+//     // Console.WriteLine("Номер задачи введен неверно");
+//     // break;
     
 }
 
@@ -71,27 +71,68 @@ static void Task47()
     }
     FillArray(array);
 }
-/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
-m = 3, n = 4.
 
-0,5 7 -2 -0,2
+static void Task50()
+{
+        // напишите прогармму, которая на вход принимает значение 
+        //элемента в двухмерном массиве, и возвращает позицию этого элемента
+        // или указание, что такого элемента нет
+        //например, задан массив
+        // 1 4 7 2
+        // 5 9 2 3
+        // 8 4 2 4
+        // 17 - такого значения нет
 
-1 -3,3 8 -9,9
+        Console.WriteLine("Введите значение элемента: ");
+        int ViewerNumber = Convert.ToInt32(Console.ReadLine());
 
-8 7,8 -7,1 9*/
+        int[,] array = new int[new Random().Next(1, 11), new Random().Next(1, 11)];
 
-/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+        void FillArray(int[,] array)
+        {
+            for ( int i = 0; i < array.GetLength(0); i++)
+            {
+            Console.WriteLine();
+            for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = new Random().Next(-10, 11);
+                    Console.Write(array[i, j] + " ");
+                }
 
-Например, задан массив:
+            }
+        }
 
-1 4 7 2
+        void Viewer(int[,] array, int ViewerNumber)
+        {
+    
+            int count = 0;
+            for ( int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] == ViewerNumber)
 
-5 9 2 3
+                    Console.WriteLine($"Искомый элемент на позиции {i}, {j}");
+                    else
+                    count++;
+                }
+                
+            }
+            if (count == array.GetLength(0) * array.GetLength(1))
+                Console.WriteLine("Искомого значения нет");
+        }
+        FillArray(array);
+        Console.WriteLine();
+        Viewer(array, ViewerNumber);
+}
 
-8 4 2 4
 
-17 -> такого числа в массиве нет*/
+
+
+
+
 
 /*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
@@ -100,3 +141,4 @@ m = 3, n = 4.
 5 9 2 3
 8 4 2 4
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
